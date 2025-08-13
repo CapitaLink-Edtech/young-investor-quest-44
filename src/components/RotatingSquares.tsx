07efadd0ad7f905e16c3bc0a1cc0e1f25ede53e9
IanEB1 <ian.estevaob@gmail.com>
2025-08-12 23:32:47 -0300

    3.1

--- src/components/RotatingSquares.tsx
+++ src/components/RotatingSquares.tsx
@@ -23,7 +23,7 @@ export const RotatingSquares = () => {
   }, []);
 
   return (
-    <div className=" fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2flex justify-center items-center gap-4 px-8">
+    <div className=" fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center gap-4 px-8">
       {positions.map((imageIndex, positionIndex) => (
         <div
           key={positionIndex}
