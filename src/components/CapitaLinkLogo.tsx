8c785d1952a912d9acc5ee69295dc43ae082d511
IanEB1 <ian.estevaob@gmail.com>
2025-08-13 09:27:26 -0300

    0.2.1

--- src/components/CapitaLinkLogo.tsx
+++ src/components/CapitaLinkLogo.tsx
@@ -1,10 +1,10 @@
 export const CapitaLinkLogo = () => {
   return (
-    <div className="fixed top-2 left-1/2 -translate-x-1/2 z-50 w-full">
+    <div className="fixed top-2 left-1/2 -translate-x-1/2 z-50">
       <img
         src="/lovable-uploads/Logo_CapitaLink.2.png"
         alt="CapitaLink Logo"
-        className="block"
+        className="block max-w-xs w-auto"
       />
     </div>
   );
